b6c8c59019a207ca8149e8cfba2c83ffe6238f45
ChethiyaWIJ <wijerathne.chethiya@gmil.com>
2025-05-26 17:43:24 +0530

    day 16 ✏️

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getProducts, getProductsById, updateProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
+productRouter.get("/:id",getProductsById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 
