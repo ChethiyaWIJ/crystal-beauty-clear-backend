be31ff40da8218644af882eabe980b95ead105ae
ChethiyaWIJ <wijerathne.chethiya@gmil.com>
2025-06-17 12:12:44 +0530

    day 19 ✏️

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 import Order from "../models/order.js";
+import Product from "../models/product.js";
 
-export function createOrder(req,res) {
+export async function createOrder(req,res) {
     if(req.user == null){
         res.status(403).json({
             message : "Unauthorized"
@@ -22,7 +23,7 @@ export function createOrder(req,res) {
         .sort({
             date : -1
         })
-        .limit(1).then((lastBills)=>{
+        .limit(1).then(async (lastBills)=>{
             if (lastBills.length == 0) {
                 orderData.orderId = "ORD0001";
             }else{
@@ -34,6 +35,25 @@ export function createOrder(req,res) {
                 const newOrderNumberInt = lastOrderNumberInt + 1;//02
                 const newOrderNumberStr = newOrderNumberInt.toString().padStart(4,'0');//"0002"
                 orderData.orderId = "ORD" + newOrderNumberStr;
+            }
+
+            for(let i = 0; i < body.billItems.length; i++){
+
+                const product = await Product.findOne({productId : body.billItems[i].productId})
+                if(product == null){
+                    res.status(403).json({
+                        message : "Product with product id " + body.billItems[i].productId + " not found"
+                    })
+                    return;
+                }
+                orderData.billItems[i] = {
+                    prodcutId : product.productId,
+                    productName : product.productName,
+                    image : product.images[0],
+                    quantity : body.billItems[i].quantity,
+                    price : product.price
+                };
+                orderData.total = orderData.total + product.price * body.billItems[i].quantity
             }
     
             const order = new Order(orderData);
@@ -90,5 +110,32 @@ export function getOrders(req,res) {
                 })
             }
         )
+    }
+}
+
+export async function changeOrder(req,res){
+    try{
+        if(req.user == null){
+            res.status(401).json({
+                message : "Unauthorized"
+            })
+            return;
+        }
+        if(req.user.role != "admin"){
+            res.status(403).json({
+                message : "You are not authorized to update an order"
+            })
+            return;
+        }
+        const orderId = req.params.orderId
+        const order = await Order.findOneAndUpdate({orderId : orderId},req.body)
+
+        res.json({
+            message : "Order updated successfully"
+        })
+    }catch(err){
+        res.status(500).json({
+            message : "Order not updated"
+        })
     }
 }
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createOrder, getOrders } from "../controllers/orderController.js";
+import { changeOrder, createOrder, getOrders } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/",createOrder);
 orderRouter.get("/",getOrders);
+orderRouter.put("/:orderId",changeOrder);
 
 export default orderRouter;
